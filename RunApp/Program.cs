d6565fa849366853d57e9ba9658eda6855ba54b7
Subin Babu <73779281+subinbab@users.noreply.github.com>
2024-11-22 14:38:52 +0530

    Update Program.cs

--- RunApp/Program.cs
+++ RunApp/Program.cs
@@ -2,7 +2,7 @@
 using Connect.Access;
 using Connect.Generate;
 
-Console.WriteLine("connecting to open ai chat bot ........");
+Console.WriteLine("connecting to open ai chat bot .........");
 
 var result = new MadeConnection();
 var chat = new GenerateText(result._azureClient,result._chatClient);
